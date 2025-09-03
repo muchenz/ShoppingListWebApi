2d4d470cb7f05f2346d4a1372d0deb5ef586058c
p c <muchenz@poczta.onet.pl>
2025-09-03 12:16:12 +0200

    fix ControllerBaseExt

--- ShoppingListWebApi/Data/ControllerBaseExt.cs
+++ ShoppingListWebApi/Data/ControllerBaseExt.cs
@@ -1,8 +1,11 @@
 ﻿using Grpc.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Shared.DataEndpoints.Models;
+using System;
+using System.Linq;
 
 namespace ShoppingListWebApi.Data;
 
@@ -11,10 +14,14 @@ public static class ControllerBaseExt
     public static ActionResult ReturnResultError(this ControllerBase controller, Result result)
     {
 
-        
+        var errors = result.GetErrors();
+
+        if (errors.All(a=>a.ErrorType == ErrorTypes.ValidationError))
+        {
+            return ValidationErrors(controller,  errors);
+        }
 
 
-        
         var error = result.GetError();
                
 
@@ -28,12 +35,26 @@ public static class ControllerBaseExt
         //    _ => new BadRequestObjectResult("Something wrong happens")
 
         //};
-        ActionResult objectResult = controller.Problem(title: error.Description, statusCode: GetHttpCode(error));
-
+        ObjectResult objectResult = controller.Problem(title: error.Description, statusCode: GetHttpCode(error));
+        
         return objectResult;
     }
 
+    private static ActionResult ValidationErrors(ControllerBase controller, Error[] errors)
+    {
+        
+        var stateDictionary = new ModelStateDictionary();
 
+        foreach (var error in errors)
+        {
+            stateDictionary.AddModelError(error.Code, error.Description);
+        }
+
+        var valProblem = controller.ValidationProblem(stateDictionary);
+
+        return valProblem;
+
+    }
 
     static int GetHttpCode(Error error)
     {
