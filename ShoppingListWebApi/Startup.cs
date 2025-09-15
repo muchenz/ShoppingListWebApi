b2025b598fdf80ab37c7878ef672969330052bcf
p c <muchenz@poczta.onet.pl>
2025-09-15 19:07:13 +0200

    add AllowCredentials to policy

--- ShoppingListWebApi/Startup.cs
+++ ShoppingListWebApi/Startup.cs
@@ -196,7 +196,7 @@ public class Startup
                 builder.AllowAnyHeader();
                 builder.AllowAnyMethod();
                 builder.WithExposedHeaders("Token-Expired");
-                //builder.AllowCredentials();
+                builder.AllowCredentials(); //for cookie
 
             });
         });
