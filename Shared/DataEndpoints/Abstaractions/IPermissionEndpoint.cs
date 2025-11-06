b61d3593885a390b1f7ab22b2219e2f875f7f676
p c <muchenz@poczta.onet.pl>
2025-11-06 19:03:11 +0100

    add DeleteUserPermission to interface

--- Shared/DataEndpoints/Abstaractions/IPermissionEndpoint.cs
+++ Shared/DataEndpoints/Abstaractions/IPermissionEndpoint.cs
@@ -4,9 +4,11 @@ using System.Threading.Tasks;
 namespace Shared.DataEndpoints.Abstaractions;
 public interface IPermissionEndpoint
 {
-    Task<Result<(User InvitedUser, Invitation Invitation)>> 
+    Task<Result<(User InvitedUser, Invitation Invitation)>>
             InviteUserPermission(int listAggregationId, int permissionLvl, string userName, string senderName, int senderId);
 
     Task<Result> ChangeUserPermission(int listAggregationId
         , UserPermissionToListAggregation item, int senderId);
+    Task<Result> DeleteUserPermission(int listAggregationId
+, UserPermissionToListAggregation item, int senderId);
 }
